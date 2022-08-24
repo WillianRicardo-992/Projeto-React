431e128c786ff4f281d403e09c8c5aecf0371361
Willian Ricardo <willianricardoleal@gmail.com>
2022-08-24 02:11:35 -0300

    Adicionando o props e outra funçao

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App(){
     return(
         <>
             <h1>Comentarios</h1>
-            <Comment />
+            <Comment title="Comentario Um" text="texto 1" author="Willian Ricardo" />
+            <Comment title="Comentario dois" text="texto 2" author="Ricardo Alberto" />
+            <Comment title="Comentario tres" text="texto 3" author="Carlos Henrique" />
         </>
     )
 }
--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import './Comment.css'
 
-function Comment() {
+function Comment(props) {
+
+    function Maiusculas(texto){
+        return texto.toUpperCase();
+    }
+    
     return (
         <>
             <section className="comment">
-                <p className="title">Titulo</p>
-                <p className="text">Texto do Comentario</p>
-                <p className="author">Autor: Willian Ricardo</p>
+                <p className="title">{Maiusculas(props.title)}</p>
+                <p className="text">{props.text}</p>
+                <p className="author">Autor:{props.author}</p>
             </section>
 
         </>
